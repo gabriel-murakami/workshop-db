9f4caa35e434ea126a6c3efb557aca46db1b5836
Gabriel Murakami <gabriel.m.alves42@gmail.com>
2026-02-17 20:23:47 -0300

    add mongodb

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,12 @@ resource "kubectl_manifest" "db_customer_service_service" {
   yaml_body  = file("${path.module}/../k8s/db-customer-service-service.yaml")
   depends_on = [kubectl_manifest.db_customer_service_deployment]
 }
+
+resource "kubectl_manifest" "mongo_db_deployment" {
+  yaml_body  = file("${path.module}/../k8s/mongo-db-deployment.yaml")
+}
+
+resource "kubectl_manifest" "mongo_db_service" {
+  yaml_body  = file("${path.module}/../k8s/mongo-db-service.yaml")
+  depends_on = [kubectl_manifest.mongo_db_deployment]
+}
